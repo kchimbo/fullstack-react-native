5d418978c125ead3ec272123e05b95b13dfe32cc
Osamu Fujimoto <osamu@caffelli.com>
2019-06-12 18:00:34 -0500

    o Working with components

--- weather/components/SearchInput.js
+++ weather/components/SearchInput.js
@@ -1,16 +1,52 @@
 import React from 'react';
 import { StyleSheet, TextInput, View } from 'react-native';
 export default class SearchInput extends React.Component {
+	// Initialize component-specific data. 
+	// The constructor fires before our component is mounted and rendered.
+	constructor(props) {
+		// super() is required in derived classes in order to reference this 
+		// within the constructor.
+		// props are immutable and are always "owned" by a component's parent 
+		// while state can be mutated and is "owned" by the component itself
+		super(props);
+		this.state = {
+			text: ''
+		};
+	}
+
+	// We need to call _this.handleChangeText.bind(this) 
+	_handleChangeText(newLocation) {
+		// ...
+	}
+	// Using property initializer. We need to call this.handleChangeText
+	// Property initializer are still in the proposal phase
+	handleChangeText = (text) => {
+		this.setState({ text });
+		// Also: this.setState({ text: text });
+	}
+	handleSubmitEditing = () => {
+		const { onSubmit } = this.props;
+		const { text } = this.state;
+		if (!text) return;
+		onSubmit(text);
+		this.setState({ text: '' });
+	}
 	render() {
+		// Do some destructuring
+		const { placeholder } = this.props;
+		const { text } = this.state;
 		return (
 			<View style={styles.container}>
 				<TextInput
 	          		autoCorrect={false}
-	          		placeholder={this.props.placeholder}
+	          		value={text}
+	          		placeholder={placeholder}
 		          	placeholderTextColor="white"
 		          	underlineColorAndroid="transparent"
 		          	style={styles.textInput}
 		          	clearButtonMode="always"
+		          	onChangeText={this.handleChangeText}
+		          	onSubmitEditing={this.handleSubmitEditing}
 	        	/>
 			</View>
 		);
