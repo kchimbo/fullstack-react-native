44b37c9c8708f7507e18b999b8b238e27c3821e2
Osamu Fujimoto <osamu.fujimoto@gmail.com>
2019-06-13 13:05:27 -0500

    o Avatar

---
+++ image-feed/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Constants } from 'expo';
+import { StyleSheet, Text, View } from 'react-native';
+import Avatar from './components/Avatar';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Avatar initials={'OF'} size={35} backgroundColor={'teal'} />
+      </View>
+    );
+  }
+}
+
+// flex: 1 - the component will expand to fill its parent entirely
+// flex: 0 - the component will shrink to the minimum space possible
+
+// flexDirection: {column, row, column-reverse, row-reverse }
+// default: column
+// Choose the primary axis.
+
+// justifyContent: { flex-start, flex-center, flex-end, space-around, space-between }
+// default: flex-start
+// Distribute children along the primary axis
+
+// alignItems: { flex-start, flex-center, flex-end, stretch }
+// default: flex-start
+// Align children along the secondary axis
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
