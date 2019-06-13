38ff28a26390011d1bea8a8e7a91bba5db844c91
Osamu Fujimoto <osamu.fujimoto@gmail.com>
2019-06-13 12:26:20 -0500

    o Add PropTypes

--- time-tracking/components/TimerButton.js
+++ time-tracking/components/TimerButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { ColorPropType, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 
 export default function TimerButton({ color, title, small, onPress}) {
 	return (
@@ -16,6 +17,17 @@ export default function TimerButton({ color, title, small, onPress}) {
 			</Text>
 		</TouchableOpacity>
 	);
+}
+
+TimerButton.propTypes = {
+	color: ColorPropType.isRequired,
+	title: PropTypes.string.isRequired,
+	small: PropTypes.bool,
+	onPress: PropTypes.func.isRequired
+};
+
+TimerButton.defaultProps = {
+	small: false,
 }
 
 const styles = StyleSheet.create({
