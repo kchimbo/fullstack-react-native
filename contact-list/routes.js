432f2aae5ab7f4224c853a60d0a060a2ca53cbbc
Osamu Fujimoto <osamu.fujimoto@gmail.com>
2019-06-14 11:03:19 -0500

    o Update Expo version. Use TabNavigator

--- contact-list/routes.js
+++ contact-list/routes.js
@@ -1,4 +1,9 @@
-import { StackNavigator } from 'react-navigation';
+import React from 'react';
+import { StackNavigator, TabNavigator } from 'react-navigation';
+// react-native-vector-icons wrapper. 
+// Creating icons is done by using JSX to define icons components. So, we
+// imported React well.
+import { MaterialIcons } from '@expo/vector-icons'
 
 import Contacts from './screens/Contacts';
 import Profile from './screens/Profile';
@@ -7,19 +12,77 @@ import User from './screens/User'
 
 import colors from './utils/colors';
 
-export default StackNavigator(
+// getTabBarIcon takes an icon string as a paremeter and returns a functions 
+// that takes the correct parameters expected by tabBarIcon.
+const getTabBarIcon = icon => ({ tintColor }) => (
+	<MaterialIcons name={icon} size={26} style={{ color: tintColor }} />
+);
+
+const ContactsScreens = StackNavigator({
+	Contacts: {
+		screen: Contacts
+	},
+	Profile: {
+		screen: Profile
+	},
+}, {
+	initialRouteName: 'Contacts',
+	// tabBarIcon expects a function. It will call that function will a single
+	// object that has the properties focused and tintColor
+	navigationOptions: {
+		tabBarIcon: getTabBarIcon('list')
+	}
+})
+
+const FavoritesScreen = StackNavigator({
+	Favorites: {
+		screen: Favorites,
+	},
+	Profile: {
+		screen: Profile,
+	},
+}, {
+	initialRouteName: 'Favorites',
+	navigationOptions: {
+		tabBarIcon: getTabBarIcon('star')
+	}
+});
+
+const UserScreens = StackNavigator({
+	User: {
+		screen: User,
+	}
+}, {
+	initialRouteName: 'User',
+	navigationOptions: {
+		tabBarIcon: getTabBarIcon('person')
+	}
+})
+
+export default TabNavigator(
 	{
-		Profile: {
-			screen: Profile,
-		},
 		Contacts: {
-			screen: Contacts,
+			screen: ContactsScreens,
+		},
+		Favorites: {
+			screen: FavoritesScreen,
 		},
 		User: {
-			screen: User
+			screen: UserScreens,
 		}
 	}, 
 	{ 
-		initialRouteName: 'User',
+		initialRouteName: 'Contacts',
+		tabBarPosition: 'bottom',
+		tabBarOptions: {
+			style: {
+				backgroundColor: colors.greyLight
+			},
+			showLabel: false,
+			showIcon: true,
+			activeTintColor: colors.blue,
+			inactiveTintColor: colors.greyDark,
+			renderIndicator: () => null,
+		}
 	},
 );
