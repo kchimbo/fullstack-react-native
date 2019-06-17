e618e1feec4f30c680bdbc9a173d626487f4bff5
Osamu Fujimoto <osamu.fujimoto@gmail.com>
2019-06-17 12:02:24 -0500

    o Rendering MessageList

--- messaging/App.js
+++ messaging/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Status from './components/Status'
+import MessageList from './components/MessageList';
+import { createImageMessage, createLocationMessage, createTextMessage } from './utils/MessageUtils';
 
 export default class App extends React.Component {
 
+  state = {
+    messages: [
+      createImageMessage('https://unsplash.it/300/300'),
+      createTextMessage('World'),
+      createTextMessage('Hello'),
+      createLocationMessage({
+        latitude: 37.78825,
+        longitude: -122.4324,
+      }),
+    ]
+  }
+
+  handlePressMessage = () => {}
+
   renderMessageList() {
+    const { messages } = this.state
     return (
-      <View style={styles.content}></View>
+      <View style={styles.content}>
+        <MessageList 
+          messages={messages}
+          onPressMessage={this.handlePressMessage}
+        />
+      </View>
     );
   }
 
